cec611e9e3d9833473f1f6f7d50d0b0b8a9e4319
born-kes <born-kes@o2.pl>
2009-09-14 10:14:07 +0200

    v 1.3.8

--- content/map.js
+++ content/map.js
@@ -1,5 +1,15 @@
-var all, table;
-var xy=document.getElementById('inputx').value+'|'+document.getElementById('inputy').value;
-all = document.evaluate('//td[@valign="top"]',document,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);
+var all, table,heddde;
+
+heddde =document.getElementsByTagName('head')[0].innerHTML += '<style type="text/css"><!--table.map div.lay4 { position: relative; bottom:38px; margin-bottom :-38}--></style>';
+all = document.evaluate('//table[@class="map"]',document,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);
+table = all.snapshotItem(0);
+var anchorTags = table.getElementsByTagName("a");
+for (var i=0; i< anchorTags.length ; i++ )
+{
+//alert(anchorTags[i].innerHTML);
+   anchorTags[i].innerHTML += '<div class="lay4"><img src="http://bornkes.w.szu.pl/img/img.php?'+anchorTags[i++].href+'" /></div>';
+
+ if(i==10){i=100;}
+ }var xy=document.getElementById('inputx').value+'|'+document.getElementById('inputy').value;
 table = all.snapshotItem(1);
 if (table.innerHTML) table.innerHTML += '<tr><td><iframe src="http://www.bornkes.w.szu.pl/pl/mt.php?_xy='+xy+'&_oko=18&t_w=on&r_w=on&obrona=on&sz_w=on" width="260" height="260" frameborder="0" name="ramka" id="ramka" style="z-index:1;">Twoja przegl1darka nie akceptuje p3ywaj1cych ramek!</iframe></td></tr>';
